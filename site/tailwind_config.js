717b118488d3e17a8f5da0944fdeefc7da42b70f
rexong <rexonghanwei200035@gmail.com>
2025-12-22 13:49:03 +0800

    add site

---
+++ site/tailwind_config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        ctp_base: "#1E1E2E",
+        ctp_mantle: "#181825",
+        ctp_crust: "#11111B",
+        ctp_text: "#CDD6F4",
+        ctp_subtext0: "#A6ADC8",
+        ctp_green: "#A6E3AD",
+        ctp_peach: "#FAB387",
+        ctp_sky: "#89DCEB",
+        ctp_mauve: "#CBA6F7",
+        ctp_sapphire: "#74C7EC",
+      },
+      fontFamily: {
+        display: ["Fira Code", "monospace"],
+      },
+      borderRadius: {
+        DEFAULT: "0.25rem",
+      },
+    },
+  },
+};
